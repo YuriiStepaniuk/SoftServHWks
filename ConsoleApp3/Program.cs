87c1c5ecfc2a894759a020018256febddae869cc
YuriiStepaniuk <88153313+YuriiStepaniuk@users.noreply.github.com>
2022-10-03 11:57:22 +0300

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HW_task_1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Type any character/word: ");
+        int spaces = 0;
+        ConsoleKey key;
+        do
+        {
+            key = Console.ReadKey().Key;
+           if (key != ConsoleKey.OemPeriod)
+            {
+                spaces++;
+            }
+        }
+        while (key != ConsoleKey.OemPeriod);
+        Console.WriteLine();
+        Console.WriteLine("Number of characters counted = {0}", spaces);
+    }
+}
+

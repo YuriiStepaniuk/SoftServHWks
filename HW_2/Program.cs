0fec39442f5e519dcd4d0496c96baa137833b390
YuriiStepaniuk <88153313+YuriiStepaniuk@users.noreply.github.com>
2022-10-03 12:02:11 +0300

    Add files via upload

---
+++ HW_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace HW_task_1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string sticket = Console.ReadLine();
+        if (sticket.Length == 6)
+        {
+
+            int ticket = Convert.ToInt32(sticket);
+            Console.WriteLine((Convert.ToInt32(Convert.ToString(sticket)[0].ToString()) +
+                               Convert.ToInt32(Convert.ToString(sticket)[1].ToString()) +
+                               Convert.ToInt32(Convert.ToString(sticket)[2].ToString())) ==
+                              (Convert.ToInt32(Convert.ToString(sticket)[3].ToString()) +
+                               Convert.ToInt32(Convert.ToString(sticket)[4].ToString()) +
+                               Convert.ToInt32(Convert.ToString(sticket)[5].ToString()))
+                ? "Ticket is lucky"
+                : "Ticket is not lucky");
+        }
+
+        else
+        {
+            Console.WriteLine("End of programm");        
+        }
+        Console.ReadLine();
+    }
+}
+
---
+++ HW_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        
+         
+        Console.WriteLine("Enter number of rows: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter number of columns: ");
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        int k = A;
+
+        if (A < B)
+            while (A <= B)
+            {
+                for (int i = 0; i < A; i++) Console.Write($"{A} ");
+                Console.WriteLine();
+                A++;
+            }
+        else
+        {
+            Console.WriteLine("Wrong input!");
+        }
+    }
+}
+
